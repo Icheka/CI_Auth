4bb20737551d7d289f9225b90c1d2e39e491718e
Icheka Ozuru <rhemafortune@gmail.com>
2021-01-01 01:32:40 +0100

    Added signin feature.

--- controllers/User.php
+++ controllers/User.php
@@ -5,6 +5,7 @@ class User extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->model("user_model");
+        $this->load->library('session');
         
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: *");
@@ -14,6 +15,8 @@ class User extends CI_Controller {
     }
     
     /**
+     * create_user(): void
+     * 
      *  HTTP Method: POST
      *  Action: Create A New User
      * 
@@ -65,6 +68,48 @@ class User extends CI_Controller {
         }
     }
     
+    /**
+     * signin(): void
+     * 
+     * HTTP Method: POST
+     * Action: Handles sign in for users
+     */
+    public function signin() {
+        $data = $this->input->post();
+        $params = array(
+            'email',
+            'password'
+            );
+        if ($this->check_params($params, $data, true) == true) {
+            // passed params check. Can continue.
+            $user = $this->admin_model->fetchUser($data['email'], 'email');
+            
+            if (count($user) == 0) {
+                // user does not exist.
+                $this->echo_error('Not Found', 'No user with that email address was found.', 404);
+                
+            } else {
+                // user does exist.
+                if (password_verify($data['password'], $user['pass']) == true) {
+                    // passwords match!
+                    // fetch user details and return.
+                    // set user_id to session
+                    $this->session->set_userdata('user_id', $user['user_id']);
+                    
+                    $this->echo_success('Success', $user);
+                } else {
+                    // passwords do not match!
+                    $this->echo_error('Password Error', 'The passwords do not match!', 401);
+                }
+            }
+        } else {
+            // failed params check. Fail gracefully.
+            $this->echo_error('Failed', 'The request was not well formed.', 406);
+            return;
+            die();
+        }
+    }
+    
     // =======================
     // PRIVATES 
     
@@ -76,11 +121,15 @@ class User extends CI_Controller {
                 'data' => $data,
                 )
             );
+        return;
+        die();
     }
     
     private function echo_error(string $header, string $message, int $code=400) {
         http_response_code($code);
         echo json_encode(array('message' => $message));
+        return;
+        die();
     }
     
     private function check_params(array $params, array $test, bool $strict) {
